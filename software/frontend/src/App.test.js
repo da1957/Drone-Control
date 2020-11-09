d0f032a407c36d7cfda59d7783521124d7d1b20c
ConnorCairns <connor@connorcairns.xyz>
2020-11-09 13:34:03 +0000

    CircleCI Integration hopefully

--- software/frontend/src/App.test.js
+++ software/frontend/src/App.test.js
@@ -1,8 +1,23 @@
-import { render, screen } from '@testing-library/react';
+import React from "react";
+import { render, unmountComponentAtNode} from "react-dom";
+import Navbar from './assets/components/navbar'
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as render target
+  container = document.createElement("div");
+  document.body.appendChild(container)
+})
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+})
+
+test('Renders navbar', () => {
+  render(<Navbar />, container);
+  expect(container.textContent).toBe("DRONE CONTROL");
+})
