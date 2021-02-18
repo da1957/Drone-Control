3c3ab3cc5e5834600e9ec349f702e2dcac0a1ce4
nisabayraktar <nisabayraktar.nb@gmail.com>
2021-02-18 16:02:31 +0000

    backend

---
+++ software/backend/src/test/java/com/dronecontrol/DronecontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dronecontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DronecontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
